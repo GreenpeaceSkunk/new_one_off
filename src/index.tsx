6cf2b3ef4a0087db2a66affd6bef9e09fc6abcc7
Dan Tovbein <dtovbein@gmail.com>
2021-01-20 12:01:18 -0300

    Fix router and implement .htaccess configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './routes/App';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router
+      basename={process.env.PUBLIC_URL}
+    >
       <App />
     </Router>
   </React.StrictMode>,
