748b94f5e1426a36adc119903f8b6038fb1b93b4
Dan Tovbein <dtovbein@gmail.com>
2021-01-20 15:24:45 -0300

    Fix conlict in Service file

--- src/routes/Registration/service.ts
+++ src/routes/Registration/service.ts
@@ -9,7 +9,6 @@ export interface IResponse {
 export const submitData = async  (data?: IUserData, refParam = `${process.env.REACT_APP_DEFAULT_REF_PARAM}`): Promise<IResponse> => {
 
   let formData = new FormData();
-
   formData.append('form_id', `${process.env.REACT_APP_FORM_ID}`);
   formData.append('ref', `${refParam}`);
 
@@ -21,8 +20,6 @@ export const submitData = async  (data?: IUserData, refParam = `${process.env.RE
     }
   }
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
   const response = await ApiCall<AxiosResquestError | any>({
     url: 'procesar_cupon.php',
     method: 'POST',
@@ -41,45 +38,6 @@ export const submitData = async  (data?: IUserData, refParam = `${process.env.RE
     return {
       submitted: true,
     };
-=======
-  // const response = await ApiCall<AxiosResquestError | any>({
-  //   url: 'procesar_cupon_en_pasos_ajax.php',
-  //   method: 'POST',
-  //   data: formData,
-  //   headers: {
-  //     'Content-Type': 'multipart/form-data'
-  //   },
-  //   params: {},
-  // });
-
-=======
-  // const response = await ApiCall<AxiosResquestError | any>({
-  //   url: 'procesar_cupon_en_pasos_ajax.php',
-  //   method: 'POST',
-  //   data: formData,
-  //   headers: {
-  //     'Content-Type': 'multipart/form-data'
-  //   },
-  //   params: {},
-  // });
-
->>>>>>> Stashed changes
-  // if(response.error) {
-  //   return {
-  //     submitted: false,
-  //   }
-  // } else {
-  //   return {
-  //     submitted: true,
-  //     post_id: response.post_id || post_id,
-  //   };
-  // }
-  return {
-    submitted: true,
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
   }
 };
 
@@ -97,7 +55,6 @@ export const submitDataWithSteps = async  (data?: IUserData, post_id?: number, r
       }
     }
   }
-console.log(formData.get('monto'));
   const response = await ApiCall<AxiosResquestError | any>({
     url: 'procesar_cupon_en_pasos_ajax.php',
     method: 'POST',
@@ -119,4 +76,3 @@ console.log(formData.get('monto'));
     };
   }
 };
-
