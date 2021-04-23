8a0c6df57b11220dccd4818890f24f49b7f07796
Dan Tovbein <dtovbein@gmail.com>
2021-04-23 06:48:03 -0300

    Add config file

---
+++ src/config.ts
@@ -0,0 +1,38 @@
+interface IConfig {
+  campaigns: {
+    [key: string] : {
+      name: string;
+      title: string;
+      description: string;
+    }
+  }
+}
+
+export default {
+  campaigns: {
+    'bosques': {
+      name: 'Bosques',
+      title: 'GREENPEACE SOS VOS',
+      description: 'Sumate antes de que el daño por desmontes e incendios generen una destrucción irreversible en los bosques',
+      image: 'bosques.jpg',
+    },
+    'clima-y-energia': {
+      name: 'Clima y energía',
+      title: 'GREENPEACE SOS VOS',
+      description: 'Sumate y en conjunto evitemos que la temperatura de nuestro planeta siga aumentando: hay alternativas',
+      image: 'clima-y-energia.jpg',
+    },
+    'contaminacion': {
+      name: 'Contaminación',
+      title: 'GREENPEACE SOS VOS',
+      description: 'Sumate antes de que sea muy tarde para salvar a nuestro planeta de los daños causados por acciones humanas',
+      image: 'contaminacion.jpg',
+    },
+    'oceanos': {
+      name: 'Océanos',
+      title: 'GREENPEACE SOS VOS',
+      description: 'Sumate para cuidar el agua, evitar que se extingan más especies, y devolver la paz a los océanos',
+      image: 'oceanos.jpg',
+    },
+  },
+} as IConfig;
